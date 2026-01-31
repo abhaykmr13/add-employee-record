0c94df16f7ddd9c34e57391b14797bb1b76002a3
abhaykmr13 <abhaykmr0121@gmail.com>
2026-01-31 11:47:25 +0530

    added exception  logic

--- src/main/java/com/example/springbootdemo/employeeService/EmployeeService.java
+++ src/main/java/com/example/springbootdemo/employeeService/EmployeeService.java
@@ -3,11 +3,11 @@ package com.example.springbootdemo.employeeService;
 
 import com.example.springbootdemo.employeeRepo.EmployeeRepository;
 import com.example.springbootdemo.entity.Employee;
+import com.example.springbootdemo.exception.EmployeeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -24,7 +24,8 @@ public class EmployeeService {
 
 
     public Employee getEmployeeData(UUID employeeId) {
-        return employeeRepository.getReferenceById(employeeId);
+        return employeeRepository.findById(employeeId).orElseThrow(() ->
+                new EmployeeNotFoundException("Employee not found with id " + employeeId));
     }
 
     public List<Employee> getAllEmployee() {
@@ -33,7 +34,7 @@ public class EmployeeService {
 
     public Employee udpateEmployee(UUID employeeId, Employee updatedEmployee) {
 
-       Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee Not Found"));
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new EmployeeNotFoundException("Employee Not Found with given Id " + employeeId));
         employee.setDepartment(updatedEmployee.getDepartment());
         employee.setId(updatedEmployee.getId());
         employee.setName(updatedEmployee.getName());
@@ -42,8 +43,12 @@ public class EmployeeService {
         return employeeRepository.save(employee);
     }
 
-    public void deleteEmployee(UUID employeeId){
-        employeeRepository.deleteById(employeeId);
+    public void deleteEmployee(UUID employeeId) {
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new EmployeeNotFoundException("The employee with given id is not there in database  " + employeeId));
+
+        employeeRepository.delete(employee);
+        //employeeRepository.deleteById(employeeId);
     }
 
 
